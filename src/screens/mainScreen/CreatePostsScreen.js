8007a3963e9d929e31cfd838e74a698ea8cba9f4
Oleksii Chyharov <chyharov@gmail.com>
2022-12-02 01:55:02 +0200

    publicate photo to PostScreen

--- src/screens/mainScreen/CreatePostsScreen.js
+++ src/screens/mainScreen/CreatePostsScreen.js
@@ -14,13 +14,40 @@ import {
 import { Camera } from "expo-camera";
 
 export default function CreatePostsScreen({ navigation }) {
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState("");
+
+  const takePhoto = async () => {
+    const photo = await camera.takePictureAsync();
+    setPhoto(photo.uri);
+    console.log("photo", photo);
+  };
+
+  const sendPhoto = () => {
+    console.log("navigation", navigation);
+    navigation.navigate("Posts", { photo });
+  };
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}>
-        <TouchableOpacity style={styles.snapContainer} onPress={() => {}}>
+      <Camera style={styles.camera} ref={setCamera}>
+        {photo && (
+          <View style={styles.takePhotoContainer}>
+            <Image
+              source={{ uri: photo }}
+              style={{ height: 200, width: 200, borderRadius: 10 }}
+            />
+          </View>
+        )}
+        <TouchableOpacity onPress={takePhoto} style={styles.snapContainer}>
           <Text style={styles.snap}>SNAP</Text>
         </TouchableOpacity>
       </Camera>
+      <View>
+        <TouchableOpacity onPress={sendPhoto} style={styles.sendBtn}>
+          <Text style={styles.sendLabel}>SEND</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -30,22 +57,46 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   camera: {
-    height: 300,
-    marginTop: 50,
-    justifyContent: "center",
+    height: "70%",
+    marginHorizontal: 10,
+    borderRadius: 10,
+    marginTop: 40,
     alignItems: "center",
+    justifyContent: "flex-end",
   },
   snap: {
     color: "#fff",
   },
   snapContainer: {
-    marginTop: 200,
     borderWidth: 1,
     borderColor: "#ff0000",
     width: 70,
     height: 70,
-    borderRadius: 50,
+    borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 20,
+  },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 50,
+    left: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  sendBtn: {
+    marginHorizontal: 30,
+    height: 40,
+    borderWidth: 2,
+    borderColor: "#20b2aa",
+    borderRadius: 10,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sendLabel: {
+    color: "#20b2aa",
+    fontSize: 20,
   },
 });
--- src/screens/mainScreen/PostsScreen.js
+++ src/screens/mainScreen/PostsScreen.js
@@ -1,21 +1,36 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  TouchableWithoutFeedback,
-  Keyboard,
-  TouchableOpacity,
-  Text,
-  ImageBackground,
-  Image,
-  Platform,
-} from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, FlatList, Image } from "react-native";
 
-export default function PostsScreen({ navigation }) {
+export default function PostsScreen({ route }) {
+  const [posts, setPosts] = useState([]);
+  console.log("route.params", route.params);
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+  console.log("posts", posts);
   return (
     <View style={styles.container}>
-      <Text style={styles.headerTitle}>PostsScreen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, indx) => indx.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginBottom: 10,
+              justifyContent: "center",
+              alignContent: "center",
+            }}
+          >
+            <Image
+              source={{ uri: item.photo }}
+              style={{ width: 350, height: 200 }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -25,7 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
   },
   headerTitle: {
     textAlign: "center",
@@ -36,5 +50,4 @@ const styles = StyleSheet.create({
     color: "#000",
     marginBottom: 33,
   },
-  iconLogOut: {},
 });
